2f08fed5d5fb4bdd89ed7270868161ee5f90c31e
renggamaulana <renggaa21@gmail.com>
2025-08-13 13:26:52 +0700

    update bdate banner

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link"
+
+export default function Navbar() {
+    return (
+        <nav className="fixed w-full text-white text-shadow-lg font-semibold text-xl top-0 left-0 flex items-center justify-between px-10 py-5 z-50">
+            <Link href="/">
+                <Image src="/icon.png" alt="logo" width={72} height={72}></Image>
+            </Link>
+            <ul className="flex items-center justify-center gap-3">
+               <li>
+                    <Link href="about">About</Link>
+               </li>
+               <li>
+                    <Link href="blog">Blog</Link>
+               </li>
+               <li>
+                    <Link href="health-tips">Health Tips</Link>
+               </li>
+            </ul>
+        </nav>
+    );
+}
