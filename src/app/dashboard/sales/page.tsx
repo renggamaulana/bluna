55cd6eb25907c9b0e85f4cfd0c6f420882abd699
renggamaulana <renggaa21@gmail.com>
2025-09-19 22:22:47 +0700

    update

--- src/app/dashboard/sales/page.tsx
+++ src/app/dashboard/sales/page.tsx
@@ -128,7 +128,7 @@ export default function SalesPage() {
                       >
                         Hapus
                       </button>
-                    </td>
+                    </td> 
                   </tr>
                 ))}
               </tbody>
